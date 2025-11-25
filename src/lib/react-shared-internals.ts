ca2d92504eebbd01241e115639f16fca05ec2c25
Simon Heuser <39399016+shsr04@users.noreply.github.com>
2025-11-25 14:19:20 +0100

    feat: add React 19 shared internals support (#49)

--- src/lib/react-shared-internals.ts
+++ src/lib/react-shared-internals.ts
@@ -2,14 +2,33 @@ import * as React from 'react'
 
 type AnyHook = (...args: any[]) => any;
 type ReactSharedInternalsType = {
+  // React 18 and below
   ReactCurrentDispatcher: {
     current?: {
       [name: string]: AnyHook
     };
   };
+} | {
+  // React 19
+  H: {
+    [name: string]: AnyHook
+  } | null
 }
 
-export const ReactSharedInternals =
-  (React as any).__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED as ReactSharedInternalsType
+export const ReactSharedInternals: ReactSharedInternalsType =
+  (React as any).__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED ??
+  (React as any).__CLIENT_INTERNALS_DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE ??
+  (React as any).__SERVER_INTERNALS_DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE;
 
-export const ReactCurrentDispatcher = ReactSharedInternals.ReactCurrentDispatcher
+function resolveDispatcher() {
+  if("ReactCurrentDispatcher" in ReactSharedInternals) {
+    return ReactSharedInternals.ReactCurrentDispatcher;
+  } else {
+    return Object.defineProperty({}, 'current', {
+      get: () => ReactSharedInternals.H ?? undefined,
+      set: (v) => { ReactSharedInternals.H = v },
+    });
+  }
+}
+
+export const ReactCurrentDispatcher = resolveDispatcher();
