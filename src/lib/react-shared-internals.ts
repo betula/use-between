08d23a7bb5a6a7d8315ee444257d74c359fa0b43
Slava Bereza <mail@betula.co>
2025-11-28 20:56:26 +0300

    chore: update react to 19

--- src/lib/react-shared-internals.ts
+++ src/lib/react-shared-internals.ts
@@ -15,20 +15,23 @@ type ReactSharedInternalsType = {
   } | null
 }
 
+/* istanbul ignore next */
 export const ReactSharedInternals: ReactSharedInternalsType =
+  // React 18 and below
   (React as any).__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED ??
-  /* istanbul ignore next */ // React 19
+  // React 19
   (React as any).__CLIENT_INTERNALS_DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE ??
-  /* istanbul ignore next */ // React 19
+  // React 19
   (React as any).__SERVER_INTERNALS_DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE ??
-  /* istanbul ignore next */ // React 19
+  // React 19
   (React as any)._DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE;
 
 function resolveDispatcher() {
   if("ReactCurrentDispatcher" in ReactSharedInternals) {
+    /* istanbul ignore next */ // React 18 and below
     return ReactSharedInternals.ReactCurrentDispatcher;
   } else {
-    /* istanbul ignore next */ // React 19
+    // React 19
     return Object.defineProperty({}, 'current', {
       get: () => ReactSharedInternals.H ?? undefined,
       set: (v) => { ReactSharedInternals.H = v },
