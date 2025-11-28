c967ccc69db4d6901058474246b3cfbb94bee8c0
Slava Bereza <mail@betula.co>
2025-11-28 20:06:42 +0300

    chore: add coverage ignore lines

--- src/lib/react-shared-internals.ts
+++ src/lib/react-shared-internals.ts
@@ -17,14 +17,18 @@ type ReactSharedInternalsType = {
 
 export const ReactSharedInternals: ReactSharedInternalsType =
   (React as any).__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED ??
+  /* istanbul ignore next */ // React 19
   (React as any).__CLIENT_INTERNALS_DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE ??
+  /* istanbul ignore next */ // React 19
   (React as any).__SERVER_INTERNALS_DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE ??
+  /* istanbul ignore next */ // React 19
   (React as any)._DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE;
 
 function resolveDispatcher() {
   if("ReactCurrentDispatcher" in ReactSharedInternals) {
     return ReactSharedInternals.ReactCurrentDispatcher;
   } else {
+    /* istanbul ignore next */ // React 19
     return Object.defineProperty({}, 'current', {
       get: () => ReactSharedInternals.H ?? undefined,
       set: (v) => { ReactSharedInternals.H = v },
